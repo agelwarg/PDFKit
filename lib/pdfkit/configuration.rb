ec5916367ab2f321d0818ce69aef16c9c7ccaad6
Adam Gelwarg <adam@gelwarg.net>
2016-06-24 00:58:27 -0400

    Remove default configuration options for wkhtmltopdf Bump version for 0.5.2.galileo.2

--- lib/pdfkit/configuration.rb
+++ lib/pdfkit/configuration.rb
@@ -6,13 +6,6 @@ class PDFKit
     def initialize
       @meta_tag_prefix = 'pdfkit-'
       @default_options_wkhtmltopdf = {
-        :disable_smart_shrinking => false,
-        :page_size => 'Letter',
-        :margin_top => '0.75in',
-        :margin_right => '0.75in',
-        :margin_bottom => '0.75in',
-        :margin_left => '0.75in',
-        :encoding => "UTF-8"
       }
       @default_options_wkhtmltoimage = {
       }
